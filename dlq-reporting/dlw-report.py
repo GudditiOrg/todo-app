af99e5e8552f2ec59abf6b1ea51a2944006bf6b5
GudditiN <ngudditi@rikoouu.com>
2023-09-12 12:57:52 +0530

    working code

--- dlq-reporting/dlw-report.py
+++ dlq-reporting/dlw-report.py
@@ -1,24 +1,28 @@
+import os
 import argparse
 import boto3
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
+from dotenv import load_dotenv
 
-def list_dlqs(session):
+load_dotenv()
+
+def list_queues(session):
     sqs_client = session.client('sqs')
     response = sqs_client.list_queues()
-    dlq_urls = []
+    queue_urls = []
 
     if 'QueueUrls' in response:
-        dlq_urls = response['QueueUrls']
+        queue_urls = response['QueueUrls']
 
-    return dlq_urls
+    return queue_urls
 
-def get_dlq_message_count(session, dlq_url):
+def get_queue_message_count(session, queue_url):
     sqs_client = session.client('sqs')
     response = sqs_client.get_queue_attributes(
-        QueueUrl=dlq_url,
+        QueueUrl=queue_url,
         AttributeNames=['ApproximateNumberOfMessages']
     )
     return int(response['Attributes']['ApproximateNumberOfMessages'])
@@ -48,29 +52,30 @@ def send_email(smtp_server, smtp_port, smtp_username, smtp_password, sender_emai
         print(f"Email could not be sent: {str(e)}")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="List DLQs and send email report")
+    parser = argparse.ArgumentParser(description="List Queues (DLQs and Normal Queues) and send email report")
     parser.add_argument("--profile", help="AWS profile name", required=True)
     args = parser.parse_args()
 
     session = boto3.Session(profile_name=args.profile)
 
-    dlqs = list_dlqs(session)
+    queues = list_queues(session)
 
-    if dlqs:
-        message = "Dead Letter Queues and Their Message Counts:\n\n"
-        for dlq_url in dlqs:
-            message += f"DLQ URL: {dlq_url}\n"
-            message += f"Message Count: {get_dlq_message_count(session, dlq_url)}\n\n"
+    if queues:
+        message = "Queues and Their Message Counts:\n\n"
+        for queue_url in queues:
+            message += f"Queue URL: {queue_url}\n"
+            message += f"Queue Name: {queue_url.split('/')[-1]}\n"
+            message += f"Message Count: {get_queue_message_count(session, queue_url)}\n\n"
 
         send_email(
-            smtp_server='smtp-relay.brevo.com',
-            smtp_port=587,  # Change if your SMTP server uses a different port
-            smtp_username='gudditinaganjaneyulu@gmail.com',
-            smtp_password='dJxTUjOLgYwD5qPm',
-            sender_email='gudditinaganjaneyulu@gmail.com',  # Your email address
-            recipient_email='gudditinaganjaneyulu@gmail.com',  # Recipient email address
-            subject='DLQ Message Count Report',
+            smtp_server=os.getenv('SMTP_SERVER'),
+            smtp_port=int(os.getenv('SMTP_PORT')),
+            smtp_username=os.getenv('SMTP_USERNAME'),
+            smtp_password=os.getenv('SMTP_PASSWORD'),
+            sender_email=os.getenv('SENDER_EMAIL'),
+            recipient_email=os.getenv('RECIPIENT_EMAIL'),
+            subject='Queue Message Count Report',
             body=message
         )
     else:
-        print("No DLQs found in the AWS account.")
+        print("No Queues found in the AWS account.")
